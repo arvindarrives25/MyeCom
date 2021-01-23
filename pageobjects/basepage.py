52147097efd2b04188f3c4ad4e7be8081d548868
arvindarrives25 <arvindarrives25@gmail.com>
2021-01-23 18:21:05 +0530

    Added exception handling code in loginpage.py file

--- pageobjects/basepage.py
+++ pageobjects/basepage.py
@@ -17,53 +17,52 @@ class BasePage:
         self.driver = driver
 
     """This is a custom click method, which will wait until the element is visible before clicking """
-    try:
-        def do_click(self, locator):
+    def do_click(self, locator):
+        try:
             WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(ec.element_to_be_clickable(locator)).click()
-    except NoSuchElementException:
-        print("Could not find the clickable element")
+        except TimeoutException:
+            print("Could not click because locator is not visible : " + str(locator))
 
     """This method will first clear the input box and then type """
-    try:
-        def do_send_keys(self, locator, text):
+    def do_send_keys(self, locator, text):
+        try:
             my_element = WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(ec.element_to_be_clickable(locator))
             my_element.click()
             my_element.clear()
             my_element.send_keys(text)
-    except TimeoutException as e:
-        print("Page load Timeout Occurred. Quiting !!!")
+        except TimeoutException:
+            print("Could not clear or type  because locator is not visible : " + str(locator))
 
     """This method will return text of the web element"""
-    try:
-        def get_text(self, locator):
+    def get_text(self, locator):
+        try:
             my_element = WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(
                 ec.visibility_of_element_located(locator))
             return my_element.text
-    except NoSuchElementException:
-        print("Could not find the locator of the text")
+        except TimeoutException:
+            print("Could not find the text because locator is not visible : " + str(locator))
 
         """This method will return if the webElement is visible or not!"""
-    try:
-        def element_visible(self, locator):
+    def element_visible(self, locator):
+        try:
             my_element = WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(
                 ec.visibility_of_element_located(locator))
             return bool(my_element)
-    except NoSuchElementException:
-        print("WebElement is not visible ")
+        except TimeoutException:
+            print("Locator is not visible : " + str(locator))
 
     """This method will return if the WebElement is enabled or not!"""
-    try:
-        def element_enabled(self, locator):
-            my_element = WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(
-                ec.visibility_of_element_located(locator))
+    def element_enabled(self, locator):
+        try:
+            my_element = WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(ec.visibility_of_element_located(locator))
             return bool(my_element)
-    except NoSuchElementException:
-        print("WebElement is not enabled")
+        except TimeoutException:
+            print("Locator is not enabled: " + str(locator))
 
     """This method will return title of the page"""
-    try:
-        def get_title(self, title):
+    def get_title(self, title):
+        try:
             WebDriverWait(self.driver, TestData.EXPLICIT_WAIT).until(ec.title_is(title))
             return self.driver.title
-    except NoSuchElementException:
-        print("Title is not present")
+        except TimeoutException:
+            print("Could not find the Title because its  not visible: ")
