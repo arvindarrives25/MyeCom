acf324eb6c380a800897cea1fb2d9dbd13a32be3
arvindarrives25 <arvindarrives25@gmail.com>
2021-02-15 00:46:46 +0530

    added

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from pathlib import Path
 
 from Configurations.config import TestData
-driver = None
+#driver = None
 
 
 # This will get the value from CLI/hooks that is browser name during run time
@@ -22,7 +22,7 @@ def pytest_addoption(parser):
 # after all tests
 @pytest.fixture(scope="class")
 def init_driver(request):
-    global driver
+    #global driver
     browser = request.config.getoption("browser")
     if browser == "chrome":
         driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -58,43 +58,43 @@ def pytest_metadata(metadata):
     metadata.pop("Plugins", None)
 
 
-@pytest.hookimpl(tryfirst=True)
-def pytest_configure(config):
-    # set custom options only if none are provided from command line
-    if not config.option.htmlpath:
-        now = datetime.now()
-        # create report target dir
-        reports_dir = Path('Reports', now.strftime('%Y%m%d'))
-        reports_dir.mkdir(parents=True, exist_ok=True)
-        # custom report file
-        report = reports_dir / f"report_{now.strftime('%H%M')}.html"
-        # adjust plugin options
-        config.option.htmlpath = report
-        config.option.self_contained_html = True
-
-
-@pytest.mark.hookwrapper
-def pytest_runtest_makereport(item):
-    """
-        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-        :param item:
-        """
-    pytest_html = item.config.pluginmanager.getplugin('html')
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
-
-    if report.when == 'call' or report.when == "setup":
-        xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(file_name)
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                extra.append(pytest_html.extras.html(html))
-        report.extra = extra
-
-
-def _capture_screenshot(name):
-    driver.get_screenshot_as_file(name)
+# @pytest.hookimpl(tryfirst=True)
+# def pytest_configure(config):
+#     # set custom options only if none are provided from command line
+#     if not config.option.htmlpath:
+#         now = datetime.now()
+#         # create report target dir
+#         reports_dir = Path('Reports', now.strftime('%Y%m%d'))
+#         reports_dir.mkdir(parents=True, exist_ok=True)
+#         # custom report file
+#         report = reports_dir / f"report_{now.strftime('%H%M')}.html"
+#         # adjust plugin options
+#         config.option.htmlpath = report
+#         config.option.self_contained_html = True
+#
+#
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item):
+#     """
+#         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+#         :param item:
+#         """
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+#
+#     if report.when == 'call' or report.when == "setup":
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             file_name = report.nodeid.replace("::", "_") + ".png"
+#             _capture_screenshot(file_name)
+#             if file_name:
+#                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+#                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
+#                 extra.append(pytest_html.extras.html(html))
+#         report.extra = extra
+#
+#
+# def _capture_screenshot(name):
+#     driver.get_screenshot_as_file(name)
